07f8a28effdc6c525d69c642f77c02aa43b311ae
NurmatovAmeer <amir8877ak@gmail.com>
2023-05-21 02:39:23 +0500

    k

--- routes/auth.js
+++ routes/auth.js
@@ -8,18 +8,18 @@ const router = express.Router();
 
 // Register
 router.post("/register", async (req, res) => {
-  const newUser = new User({
-    username: req.body.username,
-    email: req.body.email,
-    password: CryptoJS.AES.encrypt(
-      req.body.password,
-      process.env.SECRET_KEY
-    ).toString(),
-    isAdmin: req.body?.isAdmin,
-  });
+  // const newUser = new User({
+  //   username: req.body.username,
+  //   email: req.body.email,
+  //   password: CryptoJS.AES.encrypt(
+  //     req.body.password,
+  //     process.env.SECRET_KEY
+  //   ).toString(),
+  //   isAdmin: req.body?.isAdmin,
+  // });
 
   try {
-    const user = await newUser.save();
+    // const user = await newUser.save();
     return res
       .status(200)
       .json({ message: "user have succesfully registered" });
